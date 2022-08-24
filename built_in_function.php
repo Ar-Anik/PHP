37a1d64e2d8acc857438fd48a9086f5160ff50ea
Aubdur Rob Anik <aubdurrob1331@gmail.com>
2022-08-24 16:08:49 +0600

    Add files via upload

---
+++ built_in_function.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Built_in_Function </title>
+</head>
+<body>
+    
+Power : <?php echo pow(3, 2) ?> <br>
+
+Square : <?php echo sqrt(64) ?> <br>
+
+Sin : <?php echo sin(90) ?> <br>
+
+Cos : <?php echo cos(90) ?> <br>
+
+tan : <?php echo tan(90) ?> <br>
+
+rand : <?php echo rand() ?> <br>
+
+rand : <?php echo rand(1, 100) ?> <br>
+</body>
+</html>
